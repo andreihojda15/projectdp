d0dc13a202e4e6923c6bba85eda05997eae09f07
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-07 11:39:27 +0300

    [FE] Login page: implemented login page

--- react-web/src/components/Login.js
+++ react-web/src/components/Login.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../index.css';
 
 function Login() {
   return (
     <div>
-      <h1>Login</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">LandingPage</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/forgot-password">ForgotPassword</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="loginBar">
+        <h1>Login</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">LandingPage</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/forgot-password">ForgotPassword</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="loginForm">
+        <form>
+          <label htmlFor="inputEmail">
+            Email Address:
+            <input type="text" id="inputEmail" placeholder="Email" />
+          </label>
+          <label htmlFor="inputPass">
+            Password:
+            <input type="text" id="inputPass" placeholder="password" />
+          </label>
+        </form>
+      </div>
     </div>
   );
 }
