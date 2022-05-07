50f616f3069e778e358e48385cd2b39afc0d8dc3
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-07 20:45:24 +0300

    [FE] Register Page : Implemented a page to create an account

--- react-web/src/components/Register.js
+++ react-web/src/components/Register.js
@@ -1,34 +1,41 @@
 import React from 'react';
-import {
-  Link,
-} from 'react-router-dom';
+import '../index.css';
 
 function Register() {
   return (
-    <div>
-      <h1>Register Page!</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">LandingPage</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/forgot-password">ForgotPassword</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="container">
+      <div className="registerBar">
+        <h1>Sign in</h1>
+      </div>
+      <div className="registerForm">
+        <form>
+          <label htmlFor="inputFName">
+            First name:
+            <input type="text" id="inputPass" placeholder="first name" className="inputs" />
+          </label>
+          <label htmlFor="inputLName">
+            Last name:
+            <input type="text" id="inputLName" placeholder="last name" className="inputs" />
+          </label>
+          <label htmlFor="inputEmail">
+            Email Address:
+            <input type="text" id="inputEmail" placeholder="email" className="inputs" />
+          </label>
+          <label htmlFor="inputPhone">
+            Phone number:
+            <input type="text" id="inputPhone" placeholder="phone number" className="inputs" />
+          </label>
+          <label htmlFor="inputPass">
+            Password:
+            <input type="password" id="inputPass" placeholder="password" className="inputs" />
+          </label>
+          <label htmlFor="inputCPass">
+            Repeat Password:
+            <input type="password" id="inputCPass" placeholder="password" className="inputs" />
+          </label>
+          <input type="submit" value="Sign in" />
+        </form>
+      </div>
     </div>
   );
 }
