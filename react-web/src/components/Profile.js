daffe251924f9b12df0b820343b7878558400e97
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-20 21:53:07 +0300

    [FE] Courses: modified navbar

--- react-web/src/components/Profile.js
+++ react-web/src/components/Profile.js
@@ -1,32 +1,19 @@
+/* eslint-disable global-require */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../template/Navbar';
 
 function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">LandingPage</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/forgot-password">ForgotPassword</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="">
+      <Navbar value="Profile" />
+      <div className="bg-sky-200 flex justify-center gap-4">
+        <h1 className="text-4xl">Hi, user! Here you can change your password</h1>
+        <p>Your name goes here</p>
+        <Link to="/forgot-password">
+          <button type="button" className="border rounded border-black bg-white hover:bg-slate-50">Change password</button>
+        </Link>
+      </div>
     </div>
   );
 }
